efdb3f44728d0b776e5cdb85dda9db88d4e7d8fc
Anton <kalistratov.ant@gmail.com>
2020-08-13 16:58:01 +0500

    Updates

---
+++ precache-manifest.09c1f52b73917b9ad79a5ac54b836ad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec925a1328999e6e1594a93ab7521028",
+    "url": "/calc-money/index.html"
+  },
+  {
+    "revision": "28df9c9846f8ce0a4048",
+    "url": "/calc-money/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "fe0fab3a4e095c6f27ef",
+    "url": "/calc-money/static/js/2.af1665ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc-money/static/js/2.af1665ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28df9c9846f8ce0a4048",
+    "url": "/calc-money/static/js/main.c9d5006b.chunk.js"
+  },
+  {
+    "revision": "7f94107833afc5735ccc",
+    "url": "/calc-money/static/js/runtime-main.b19a42aa.js"
+  }
+]);
